15715f80c495ec93c48584de54dd5ca038561fd6
Kevin Gary <kevgary@Kevins-MacBook-Pro.local>
2016-07-07 00:36:15 -0600

    form layout with model bindins in place. displaying data

--- angular/controllers/global.js
+++ angular/controllers/global.js
@@ -1,3 +1,69 @@
 app.controller('Global', function($scope) {
-	$scope.hello = 'yo';
+	//init
+	// $scope.nUrls = 1;
+	// $scope.nRCs = 1;
+
+	//populate array of n length
+	// $scope.populateArray = function(num) {
+	// 	return new Array(num);
+	// }
+	var urlInput = {
+		value: '',
+		placeholder: 'URL'
+	};
+	var rcInput = {
+		request: {
+			value: 0,
+			placeholder: 'req'
+		},
+		concurrency: {
+			value: [],
+			placeholder: 'curr'
+		}
+	};
+	var headerInput = {
+		value: '',
+		placeholder: 'Header'
+	};
+
+	$scope.formData = {
+		urls: [urlInput],
+		rcs: [rcInput],
+		headers: [headerInput]
+	};
+	$scope.addInput = function(param) {
+		switch(param) {
+			case 'url':
+				$scope.formData.urls.push({
+					value: 0,
+					placeholder: 'URL'
+				});
+				break;
+			case 'rc':
+				$scope.formData.rcs.push({
+					request: {
+						value: 0,
+						placeholder: 'req'
+					},
+					concurrency: {
+						value: [],
+						placeholder: 'curr'
+					}
+				});
+				break;
+			case 'header':
+				$scope.formData.headers.push({
+					value: '',
+					placeholder: 'Header'
+				});
+				break;
+			default:
+				console.error('void addInput');
+				break;
+		};
+	};
+
+	$scope.stringify = function(param) {
+		return JSON.stringify(param, undefined, 2);
+	}
 });
